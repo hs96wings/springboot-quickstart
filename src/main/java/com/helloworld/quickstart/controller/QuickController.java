46e7bbdf2f47739080250d8762b511fecd94d84f
hs96wings <hs96wings@gmail.com>
2024-07-26 12:23:33 +0900

    @GetMapping 활용

--- src/main/java/com/helloworld/quickstart/controller/QuickController.java
+++ src/main/java/com/helloworld/quickstart/controller/QuickController.java
@@ -2,6 +2,8 @@ package com.helloworld.quickstart.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +19,16 @@ public class QuickController {
         log.info("dummy2");
         return "dummy2";
     }
+    @GetMapping("/member")
+    public String getMember(@RequestParam("empNo") String empNo, @RequestParam("year") int year) {
+        log.info("empNo: {}", empNo);
+        log.info("year: {}", year);
+        return "ok";
+    }
+    @GetMapping("/company/{id}")
+    public String getCompany(@PathVariable("id") String id) {
+        log.info("id: {}", id);
+        return "ok";
+    }
 }
+
