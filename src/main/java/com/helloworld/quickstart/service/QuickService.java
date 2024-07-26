3c6fc56fce6f0d9e1d23813b585968c7ec186cf5
hs96wings <hs96wings@gmail.com>
2024-07-26 17:52:43 +0900

    JPA 설정 및 활용

--- src/main/java/com/helloworld/quickstart/service/QuickService.java
+++ src/main/java/com/helloworld/quickstart/service/QuickService.java
@@ -1,7 +1,9 @@
 package com.helloworld.quickstart.service;
 
 import com.helloworld.quickstart.dto.ItemDto;
+import com.helloworld.quickstart.entity.ItemEntity;
 import com.helloworld.quickstart.mapper.QuickMapper;
+import com.helloworld.quickstart.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,29 +13,45 @@ import java.util.HashMap;
 @Service
 @Slf4j
 public class QuickService {
-
     @Autowired
     private QuickMapper quickMapper;
+    @Autowired
+    private ItemRepository itemRepository;
     public boolean registerItem(ItemDto itemDto) {
         // TODO: DB insert
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("id", itemDto.getId());
-        paramMap.put("name", itemDto.getName());
-        quickMapper.registerItem(paramMap);
-        log.info("service ...");
+//        MyBatis
+//        HashMap<String, Object> paramMap = new HashMap<>();
+//        paramMap.put("id", itemDto.getId());
+//        paramMap.put("name", itemDto.getName());
+//        quickMapper.registerItem(paramMap);
+//        log.info("service ...");
+
+//        JPA
+        ItemEntity itemEntity = new ItemEntity();
+        itemEntity.setId(itemDto.getId());
+        itemEntity.setName(itemDto.getName());
+        itemRepository.save(itemEntity);
         return true;
     }
 
     public ItemDto getItemById(String id) {
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("id", id);
+//        MyBatis
+//        HashMap<String, Object> paramMap = new HashMap<>();
+//        paramMap.put("id", id);
+//
+//        HashMap<String, Object> res = quickMapper.findById(paramMap);
+//
+//        ItemDto itemDto = new ItemDto();
+//        itemDto.setId((String)res.get("ID"));
+//        itemDto.setName((String)res.get("NAME"));
+//
+//        return itemDto;
 
-        HashMap<String, Object> res = quickMapper.findById(paramMap);
-
+//        JPA
+        ItemEntity itemEntity = itemRepository.findById(id).get();
         ItemDto itemDto = new ItemDto();
-        itemDto.setId((String)res.get("ID"));
-        itemDto.setName((String)res.get("NAME"));
-
+        itemDto.setId(itemEntity.getId());
+        itemDto.setName(itemEntity.getName());
         return itemDto;
     }
 }
