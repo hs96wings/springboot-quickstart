9434d05fc7ccb94c783baa15a41b43fc88f8560f
hs96wings <hs96wings@gmail.com>
2024-07-26 12:16:51 +0900

    lombok 설정

--- src/main/java/com/helloworld/quickstart/controller/QuickController.java
+++ src/main/java/com/helloworld/quickstart/controller/QuickController.java
@@ -1,16 +1,20 @@
 package com.helloworld.quickstart.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class QuickController {
     @GetMapping("/dummy")
     public String dummy() {
+        log.info("dummy");
         return "{}";
     }
     @GetMapping("/dummy2")
     public String dummy2() {
+        log.info("dummy2");
         return "dummy2";
     }
 }
